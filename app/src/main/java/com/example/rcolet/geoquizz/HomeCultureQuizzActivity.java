55d4e80f99b282614067e25305ee09c174b7ff09
Remi Colet <remi.colet@bayard-presse.com>
2019-01-08 15:59:39 +0100

    Create 7 empty activities

--- app/src/main/java/com/example/rcolet/geoquizz/HomeCultureQuizzActivity.java
+++ app/src/main/java/com/example/rcolet/geoquizz/HomeCultureQuizzActivity.java
@@ -1,7 +1,9 @@
 package com.example.rcolet.geoquizz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeCultureQuizzActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class HomeCultureQuizzActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_culture_quizz);
+    }
+
+    protected void startGameCultureQuizz(View view) {
+        Intent startCultureQuizz = new Intent(this, CultureQuizzActivity.class);
+        startActivity(startCultureQuizz);
+    }
+
+    protected void hightScoreCultureQuizz(View view) {
+        Intent hightScoreCultureQuizz = new Intent(this, ScoreCultureQuizzActivity.class);
+        startActivity(hightScoreCultureQuizz);
     }
 }
--- app/src/main/java/com/example/rcolet/geoquizz/HomeMapQuizzActivity.java
+++ app/src/main/java/com/example/rcolet/geoquizz/HomeMapQuizzActivity.java
@@ -1,7 +1,9 @@
 package com.example.rcolet.geoquizz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeMapQuizzActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class HomeMapQuizzActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_map_quizz);
+    }
+
+    protected void startGameMapQuizz(View view) {
+        Intent startMapQuizz = new Intent(this, MapQuizzActivity.class);
+        startActivity(startMapQuizz);
+    }
+
+    protected void hightScoreMapQuizz(View view) {
+        Intent hightScoreMapQuizz = new Intent(this, ScoreMapQuizzActivity.class);
+        startActivity(hightScoreMapQuizz);
     }
 }
