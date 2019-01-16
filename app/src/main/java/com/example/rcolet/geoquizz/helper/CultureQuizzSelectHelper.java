6c769b148a77bb34cc4f5e5e451e4e1045217e07
qj141460 <jules.quesney@etu.univ-lehavre.fr>
2019-01-16 14:47:03 +0100

    working prototype of quizz done (ask only country and rework to do)

--- app/src/main/java/com/example/rcolet/geoquizz/helper/CultureQuizzSelectHelper.java
+++ app/src/main/java/com/example/rcolet/geoquizz/helper/CultureQuizzSelectHelper.java
@@ -3,6 +3,8 @@ package com.example.rcolet.geoquizz.helper;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.support.design.widget.TabLayout;
+import android.util.Log;
 
 import java.util.Random;
 
@@ -12,10 +14,17 @@ public class CultureQuizzSelectHelper {
     private static SQLiteDatabase sqlitedb = null;
     private static DDBhelper myddb = null;
 
+    private static String answerNum1 = null;
+    private static String answerNum2 = null;
+    private static String answerNum3 = null;
+    private static String answerNum4 = null;
+    private static int goodanswer = 0;
+
+
     public CultureQuizzSelectHelper(Context context) {
         this.context = context;
         myddb = new DDBhelper(this.context);
-        sqlitedb = myddb.getWritableDatabase();
+        sqlitedb = myddb.getReadableDatabase();
     }
 
     public static String SelectQuestion(){
@@ -32,14 +41,24 @@ public class CultureQuizzSelectHelper {
         Cursor all = sqlitedb.rawQuery("select * from "+DDBhelper.TABLE_NAME,null);
 
 
-        int row = r.nextInt(all.getCount()+1);
+        int row = r.nextInt(all.getCount())+1;
 
-        int col1 = r.nextInt(3+1); // Pays, Capital, NbHab
+        //int col1 = r.nextInt(3); // Pays, Capital, NbHab
+        int col1 = 0; // Pays uniquement //TEST
+
         int col2 = col1;
         while(col2==col1)
         {
-            col2 = r.nextInt(3+1); // Pays, Capital, NbHab
+            col2 = r.nextInt(3); // Pays, Capital, NbHab
         }
+
+        goodanswer = r.nextInt(4) +1; // [1-4], 0 est réservé pour l'erreur
+
+
+        Log.i("TAG", "num of random row : " + row);
+        Log.i("TAG", "num of random col1 : " + col1);
+        Log.i("TAG", "num of random col2 : " + col2);
+
 
         //TODO
         //Cursor answer = sqlitedb.rawQuery("select * from "+DDBhelper.TABLE_NAME,null);
@@ -58,6 +77,135 @@ public class CultureQuizzSelectHelper {
         if(col1==0)
         {
             sRet+="Quel est le Pays ";
+
+            int randomPick = r.nextInt(all.getCount()) +1;
+            /*
+            for(int i = 0; i<4; i++)
+            {
+                if(i+1 != goodanswer)
+                {
+                    //...
+                }
+                else
+                {
+                    if(goodanswer==1)
+                    {
+                        answerNum1=all.getString(col1);
+                    }
+                    if(goodanswer==2)
+                    {
+                        answerNum2=all.getString(col1);
+                    }
+                    if(goodanswer==3)
+                    {
+                        answerNum3=all.getString(col1);
+                    }
+                    if(goodanswer==4)
+                    {
+                        answerNum4="";
+                    }
+                }
+            }
+            */
+
+            // 1
+            while(randomPick==row)
+            {
+                randomPick = r.nextInt(all.getCount()) +1;
+            }
+
+            Log.i("TAG", "num of random randomPick : " + randomPick);
+
+
+            Cursor cu = sqlitedb.rawQuery("select * from "+DDBhelper.TABLE_NAME,null);
+            int i1 = 0;
+            while(i1!=randomPick)
+            {
+                cu.moveToNext();
+                i1++;
+            }
+
+            Log.i("TAG", "num of random i1 : " + i1);
+
+
+            answerNum1=cu.getString(col1+1);
+
+            // 2
+            while(randomPick==row || randomPick==i1)
+            {
+                randomPick = r.nextInt(all.getCount()) +1;
+            }
+
+            cu = sqlitedb.rawQuery("select * from "+DDBhelper.TABLE_NAME,null);
+            int i2 = 0;
+            while(i2!=randomPick)
+            {
+                cu.moveToNext();
+                i2++;
+            }
+
+            Log.i("TAG", "num of random i2 : " + i2);
+
+            answerNum2=cu.getString(col1+1);
+
+            // 3
+            while(randomPick==row || randomPick==i1 || randomPick==i2)
+            {
+                randomPick = r.nextInt(all.getCount()) +1;
+            }
+
+            cu = sqlitedb.rawQuery("select * from "+DDBhelper.TABLE_NAME,null);
+            int i3 = 0;
+            while(i3!=randomPick)
+            {
+                cu.moveToNext();
+                i3++;
+            }
+
+            Log.i("TAG", "num of random i3 : " + i3);
+
+            answerNum3=cu.getString(col1+1);
+
+            // 4
+            while(randomPick==row || randomPick==i1 || randomPick==i2 || randomPick==i3)
+            {
+                randomPick = r.nextInt(all.getCount()) +1;
+            }
+
+            cu = sqlitedb.rawQuery("select * from "+DDBhelper.TABLE_NAME,null);
+            int i4 = 0;
+            while(i4!=randomPick)
+            {
+                cu.moveToNext();
+                i4++;
+            }
+
+            Log.i("TAG", "num of random i4 : " + i4);
+
+
+            answerNum4=cu.getString(col1+1);
+
+
+            if(goodanswer==1)
+            {
+                answerNum1=all.getString(col1+1);
+            }
+            if(goodanswer==2)
+            {
+                answerNum2=all.getString(col1+1);
+            }
+            if(goodanswer==3)
+            {
+                answerNum3=all.getString(col1+1);
+            }
+            if(goodanswer==4)
+            {
+                answerNum4=all.getString(col1+1);
+            }
+
+            Log.i("TAG", "GoodAnswer : " + goodanswer);
+
+
         }
         if(col1==1)
         {
@@ -90,6 +238,33 @@ public class CultureQuizzSelectHelper {
 
 
         return sRet;
+    }
+
+
+    /* --- Getter --- */
+    public static String getAnswer1()
+    {
+        return answerNum1;
+    }
+
+    public static String getAnswer2()
+    {
+        return answerNum2;
+    }
+
+    public static String getAnswer3()
+    {
+        return answerNum3;
+    }
+
+    public static String getAnswer4()
+    {
+        return answerNum4;
+    }
+
+    public static int getGoodAnswer()
+    {
+        return goodanswer;
     }
 
 }
--- app/src/main/java/com/example/rcolet/geoquizz/quizz/CultureQuizzActivity.java
+++ app/src/main/java/com/example/rcolet/geoquizz/quizz/CultureQuizzActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.rcolet.geoquizz.R;
@@ -12,9 +13,19 @@ import com.example.rcolet.geoquizz.score.ScoreCultureQuizzActivity;
 
 public class CultureQuizzActivity extends AppCompatActivity {
 
-    public static int counter = 0;
+    public static int counter = 1;
+
+    public static int goodAnswer = 0;
+
+
     TextView question;
     CultureQuizzSelectHelper CQSH = null;
+
+    Button b1 = null;
+    Button b2 = null;
+    Button b3 = null;
+    Button b4 = null;
+
 
 
     @Override
@@ -26,30 +37,77 @@ public class CultureQuizzActivity extends AppCompatActivity {
         CQSH = new CultureQuizzSelectHelper(this);
 
         question = (TextView)findViewById(R.id.question);
+
+        b1 = (Button)findViewById(R.id.answer1);
+        b2 = (Button)findViewById(R.id.answer2);
+        b3 = (Button)findViewById(R.id.answer3);
+        b4 = (Button)findViewById(R.id.answer4);
+
+
         //question.setText("Question " + counter);
 
-        question.setText(CQSH.SelectQuestion());
+        question.setText(" Question n°" + counter + " (" + goodAnswer + "): " + CQSH.SelectQuestion());
+
+        b1.setText(CQSH.getAnswer1());
+        b2.setText(CQSH.getAnswer2());
+        b3.setText(CQSH.getAnswer3());
+        b4.setText(CQSH.getAnswer4());
+
+
 
     }
 
     protected void answer1(View view) {
+
+        String str = Integer.toString(CQSH.getGoodAnswer());
+
+        if(str.charAt(str.length() - 1) == '1')
+        {
+            goodAnswer++;
+        }
+
         answerAction(view);
     }
 
     protected void answer2(View view) {
+
+        String str = Integer.toString(CQSH.getGoodAnswer());
+
+        if(str.charAt(str.length() - 1) == '2')
+        {
+            goodAnswer++;
+        }
+
         answerAction(view);
     }
 
     protected void answer3(View view) {
+
+        String str = Integer.toString(CQSH.getGoodAnswer());
+
+        if(str.charAt(str.length() - 1) == '3')
+        {
+            goodAnswer++;
+        }
+
         answerAction(view);
     }
 
     protected void answer4(View view) {
+
+        String str = Integer.toString(CQSH.getGoodAnswer());
+
+        if(str.charAt(str.length() - 1) == '4')
+        {
+            goodAnswer++;
+        }
+
         answerAction(view);
     }
 
     public void answerAction(View view){
         counter++;
+
         Intent startCultureQuizz = new Intent(this, CultureQuizzActivity.class);
         startCultureQuizz.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
         startActivity(startCultureQuizz);
